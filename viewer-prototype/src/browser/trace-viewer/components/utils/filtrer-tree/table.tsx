9622e8c93b2f7055daac9877fb3e4ef55bf93499
Lea <lea.carlier@polymtl.ca>
2020-08-03 15:21:33 -0400

    add dynamic table
    Signed-off-by: Lea <lea.carlier@polymtl.ca>

--- viewer-prototype/src/browser/trace-viewer/components/utils/filtrer-tree/table.tsx
+++ viewer-prototype/src/browser/trace-viewer/components/utils/filtrer-tree/table.tsx
@@ -1,33 +1,56 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import * as React from 'react';
-import { Entry } from 'tsp-typescript-client/lib/models/entry';
+import { TreeNode } from './tree-node';
 
 interface TableProps {
-    entries: Entry[];
+    nodes: TreeNode[];
 }
 
 export class Table extends React.Component<TableProps> {
+    private keys: string[] = [];
+    private flatTree: TreeNode[] = [];
+
     constructor(props: TableProps) {
         super(props);
+        this.flatTree = this.flattenTree(this.props.nodes);
     }
 
+    flattenTree = (nodes: TreeNode[]): TreeNode[] => {
+        let flatTree: TreeNode[] = [];
+        nodes.forEach((node: TreeNode) => {
+            flatTree.push(node);
+            if (node.children.length) {
+                flatTree = flatTree.concat(...this.flattenTree(node.children));
+            }
+        });
+        return flatTree;
+    };
+
     renderTableHeader(): React.ReactNode {
-        const header = ['Process', 'TID', 'Time', 'Legend'];
-        return header.map((title, index) => <th key={index}>{title}</th>);
+        const keys = Object.keys(this.props.nodes[0]);
+        const excludeKeys = ['isRoot', 'children', 'style', 'metadata', 'labels', 'id', 'parentId'];
+        const header = keys.filter(key => !excludeKeys.includes(key)).concat('Legend');
+        this.keys = header;
+        return header.map((title, index) => <th key={index}>{this.toCapitalCase(title)}</th>);
+    }
+
+    toCapitalCase = (name: string): string => (name.charAt(0).toUpperCase() + name.slice(1));
+
+    renderRow(node: TreeNode): React.ReactNode {
+        return this.keys.map((key: string, index) => {
+            if (key === 'Legend') {
+                return <td key={node.id+'-'+index}></td>;
+            } else {
+                return <td key={node.id+'-'+index}>{node[key as keyof TreeNode]}</td>;
+            }
+        });
     }
 
     renderTableData(): React.ReactNode {
-        return this.props.entries.map((entry: Entry) => {
-            const { id, name, tid, time } = (entry as any);
-            return (
-                <tr key={id}>
-                    <td>{name}</td>
-                    <td>{tid}</td>
-                    <td>{time}</td>
-                    <td>{}</td>
+        return this.flatTree.map((node: TreeNode) => (
+                <tr key={node.id}>
+                    {this.renderRow(node)}
                 </tr>
-            );
-        });
+            ));
     }
 
     render(): JSX.Element {
