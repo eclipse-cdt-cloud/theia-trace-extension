9d7f831497151db622325a2841ed880d355af25f
muddana-satish <satish.muddana@ericsson.com>
2020-10-05 02:39:14 -0400

    Remove state selection
    This logic is already handled in timeline-chart. And validating states with id is incorrect. Hence removed it from this file.
    
    Signed-off-by: muddana-satish <satish.muddana@ericsson.com>

--- viewer-prototype/src/browser/trace-viewer/components/timegraph-output-component.tsx
+++ viewer-prototype/src/browser/trace-viewer/components/timegraph-output-component.tsx
@@ -228,15 +228,6 @@ export class TimegraphOutputComponent extends AbstractTreeOutputComponent<Timegr
         const newRange: TimelineChart.TimeGraphRange = { start, end };
         const newResolution: number = resolution * 0.8;
         const timeGraphData: TimelineChart.TimeGraphModel = await this.tspDataProvider.getData(orderedTreeIds, this.state.timegraphTree, newRange, this.props.style.chartWidth);
-        if (timeGraphData && this.selectedElement) {
-            for (const row of timeGraphData.rows) {
-                const selEl = row.states.find(el => !!this.selectedElement && el.id === this.selectedElement.id);
-                if (selEl) {
-                    selEl.selected = true;
-                    break;
-                }
-            }
-        }
         return {
             rows: timeGraphData ? timeGraphData.rows : [],
             range: newRange,
