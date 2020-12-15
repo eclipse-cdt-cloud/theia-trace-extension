52d80ff6d496f1882f5550591131315d36604dba
muddana-satish <satish.muddana@ericsson.com>
2020-12-15 00:02:54 -0800

    Sync event timestamp with timegraph
    Signed-off-by: muddana-satish <satish.muddana@ericsson.com>

--- packages/base/src/signal-manager.ts
+++ packages/base/src/signal-manager.ts
@@ -4,27 +4,33 @@ export declare interface SignalManager {
 
     fireTooltipSignal(tooltip: { [key: string]: string }): void;
     fireThemeChangedSignal(theme: string): void;
+    fireSelectionChangedSignal(payload: { [key: string]: string }): void;
 
 }
 
 export const Signals = {
-    TRACE_OPENED : 'trace opened',
-    TRACE_CLOSED : 'trace closed',
+    TRACE_OPENED: 'trace opened',
+    TRACE_CLOSED: 'trace closed',
     EXPERIMENT_OPENED: 'experiment opened',
     EXPERIMENT_CLOSED: 'experiment closed',
     EXPERIMENT_SELECTED: 'experiment selected',
     TOOLTIP_UPDATED: 'tooltip updated',
-    THEME_CHANGED: 'theme changed'
+    THEME_CHANGED: 'theme changed',
+    SELECTION_CHANGED: 'selection changed'
 };
 
 export class SignalManager extends EventEmitter implements SignalManager {
 
     fireTooltipSignal(tooltip: { [key: string]: string; }): void {
-        this.emit(Signals.TOOLTIP_UPDATED, {tooltip});
+        this.emit(Signals.TOOLTIP_UPDATED, { tooltip });
     }
 
     fireThemeChangedSignal(theme: string) {
         this.emit(Signals.THEME_CHANGED, theme);
+    }
+
+    fireSelectionChangedSignal(payload: { [key: string]: string; }): void {
+        this.emit(Signals.SELECTION_CHANGED, { payload });
     }
 
 }
