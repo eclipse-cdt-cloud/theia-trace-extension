1e33812db1d935de7824dac4811221ab3a0ca87e
Ankush Tyagi <ankush.tyagi@ericsson.com>
2021-01-15 23:14:02 +0100

    Support error handling for start/stop trace server
    Signed-off-by: Ankush Tyagi <ankush.tyagi@ericsson.com>

--- viewer-prototype/src/common/trace-server-config.ts
+++ viewer-prototype/src/common/trace-server-config.ts
@@ -1,15 +1,20 @@
+import { ApplicationError } from '@theia/core';
 
 export const traceServerPath = '/services/trace-server-config';
 export const TraceServerConfigService = Symbol('TraceServerConfigService');
+export const PortBusy = ApplicationError.declare(-32650, code => ({
+    message: 'Port busy',
+    data: { code }
+}));
 export interface TraceServerConfigService {
     /**
      * Spawn the trace server from a given path
      */
-    startTraceServer(path: string | undefined, port: number | undefined): Promise<void>;
+    startTraceServer(path: string | undefined, port: number | undefined): Promise<string>;
 
     /**
      * Stop the trace server
      */
-    stopTraceServer(port: number | undefined): Promise<void>;
+    stopTraceServer(): Promise<string>;
 }
 
--- viewer-prototype/src/node/trace-server-service.ts
+++ viewer-prototype/src/node/trace-server-service.ts
@@ -1,33 +1,56 @@
-import { spawn, exec } from 'child_process';
+import { spawn } from 'child_process';
 import { injectable } from 'inversify';
-import { TraceServerConfigService } from '../common/trace-server-config';
+import { PortBusy, TraceServerConfigService } from '../common/trace-server-config';
+import treeKill = require('tree-kill');
 @injectable()
 export class TraceServerServiceImpl implements TraceServerConfigService {
+    private processId: number;
 
-    startTraceServer(path: string | undefined, port: number | undefined): Promise<void> {
+    startTraceServer(path: string | undefined, port: number | undefined): Promise<string> {
         const server = spawn(`${path}`, ['-vmargs', `-Dtraceserver.port=${port}`]);
-        return new Promise<void>((resolve, reject) => {
-            // If the server provides output on any channel before it exits, consider that a success.
+        this.processId = server.pid;
+        const timeouts: NodeJS.Timeout[] = [];
+
+        return new Promise<string>((resolve, reject) => {
+            // TODO: Since the resolve never actually gets returned, we need a better way to determine if the child process
+            // initiated by the spawn successfully starts the trace server
+
+            // If the server doesn't error or exit within 2 seconds, we consider that a success.
             // That doesn't mean that the server has really started. On the frontend, the `TraceServerConnectionStatusService`
             // will ping the port until it receives a response, which is the official measure of success.
-            server.stdout.on('data', () => resolve());
-            server.stderr.on('data', () => resolve());
+
+            timeouts.push(setTimeout(() => resolve('success'), 2000));
+
             // If the server exits or errors before it outputs, consider it a failure.
-            server.on('exit', code => reject(code));
-            server.on('error', error => reject(error));
-        })
-            .finally(() => { server.removeAllListeners(); });
+            server.on('error', err => {
+                reject(err);
+            });
+            server.on('exit', code => {
+                reject(PortBusy(code));
+            });
+            // If no response recieved from the trace server in 10 seconds, reject with an error (for internal use)
+            timeouts.push(setTimeout(() => reject('Waited 10 seconds but nothing happened and hence exiting'), 10000));
+
+        }).finally(() => {
+            server.removeAllListeners();
+            timeouts.forEach(timeout => clearTimeout(timeout));
+        });
+
     }
 
-    stopTraceServer(port: number | undefined): Promise<void> {
-        const terminator = exec(`kill -9 $(lsof -t -i:${port} -sTCP:LISTEN)`);  // FIXME: Better approach to kill the server at a given port
-        return new Promise((resolve, reject) => {
-            terminator.on('exit', code => {
-                if (code === 0 || code === 1) {
-                    resolve();
-                } else {
-                    reject(code);
+    stopTraceServer(): Promise<string> {
+        return new Promise<string>((resolve, reject) => {
+            if (this.processId === -1 || !this.processId) {
+                reject('process already killed or process hasn\'t been started');
+                return;
+            }
+            treeKill(this.processId, error => {
+                this.processId = -1;
+                if (error) {
+                    reject(error);
+                    return;
                 }
+                resolve('success');
             });
         });
     }
