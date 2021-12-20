2989d9b11021c44a9392ef4ae7c9c3b1ab224679
Patrick Tasse <patrick.tasse@ericsson.com>
2021-12-20 09:23:38 -0500

    Make filter width match first column width
    Add resize observer on first column header to synchronize filter width.
    
    Remove text message on empty filter result as this disrupts the observer
    and prevents synchronization after the non-matching filter is removed.
    
    Signed-off-by: Patrick Tasse <patrick.tasse@ericsson.com>

--- packages/react-components/src/components/utils/filtrer-tree/filter.tsx
+++ packages/react-components/src/components/utils/filtrer-tree/filter.tsx
@@ -4,17 +4,45 @@ interface FilterProps {
     onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-export class Filter extends React.Component<FilterProps> {
+interface FilterState {
+    width: number
+}
+export class Filter extends React.Component<FilterProps, FilterState> {
+
+    private ref: React.RefObject<HTMLDivElement>;
+    private resizeObserver: ResizeObserver;
+
     constructor(props: FilterProps) {
         super(props);
+        this.ref = React.createRef();
+        this.state = {
+            width: 0
+        };
+        this.resizeObserver = new ResizeObserver(entries => {
+            this.setState({
+                width: entries[0].contentRect.width
+            });
+        });
+    }
+
+    componentDidMount(): void {
+        const header = this.ref.current?.parentElement?.querySelector('th');
+        if (header) {
+            this.resizeObserver.observe(header);
+        }
+    }
+
+    componentWillUnmount(): void {
+        this.resizeObserver.disconnect();
     }
 
     render(): JSX.Element {
-        return <div onChange={this.props.onChange}>
+        return <div ref={this.ref} onChange={this.props.onChange}>
             <input
                 id="input-filter-tree"
                 type="text"
                 placeholder="Filter"
+                style={{width: this.state.width}}
             />
         </div>;
     }
