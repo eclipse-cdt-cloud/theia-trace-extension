2fb88b4e0e8d2b9f4550b83243ac5566ffc861d2
hriday-panchasara <hriday.panchasara@ericsson.com>
2022-02-21 13:10:05 -0600

    Support displaying statistics for a region selection in the chart
    Show statistics for selected region in datatree-output-component
    
    Signed-off-by: hriday-panchasara <hriday.panchasara@ericsson.com>

--- packages/react-components/src/components/utils/filter-tree/tree.tsx
+++ packages/react-components/src/components/utils/filter-tree/tree.tsx
@@ -6,6 +6,7 @@ import { Table } from './table';
 import { getAllExpandedNodeIds } from './utils';
 import { SortConfig, sortNodes } from './sort';
 import ColumnHeader from './column-header';
+import { isEqual } from 'lodash';
 
 interface FilterTreeProps {
     nodes: TreeNode[];
@@ -43,6 +44,8 @@ export class FilterTree extends React.Component<FilterTreeProps, FilterTreeState
             sortConfig: []
         };
     }
+
+    private _filter = '';
 
     getRootNodes = (): TreeNode[] => {
         const nodes = [...this.props.nodes];
@@ -211,6 +214,7 @@ export class FilterTree extends React.Component<FilterTreeProps, FilterTreeState
     isCollapsed = (id: number): boolean => this.props.collapsedNodes.includes(id);
 
     handleFilterChanged = (filter: string): void => {
+        this._filter = filter;
         let filteredTree: TreeNode[] = [];
         const matchedIds: number[] = [];
         const rootNodes = this.getRootNodes();
@@ -261,8 +265,7 @@ export class FilterTree extends React.Component<FilterTreeProps, FilterTreeState
             onSortConfigChange={this.handleSortConfigChange}
             showHeader={this.props.showHeader}
             headers={this.props.headers}
-            className={this.props.className}
-        />;
+            className={this.props.className} />;
 
     render(): JSX.Element | undefined {
         if (!this.props.nodes) { return undefined; }
@@ -277,6 +280,12 @@ export class FilterTree extends React.Component<FilterTreeProps, FilterTreeState
             </React.Fragment>;
         } else {
             return <Message></Message>;
+        }
+    }
+
+    componentDidUpdate(prevProps: FilterTreeProps): void {
+        if (!isEqual(this.props.nodes, prevProps.nodes)) {
+            this.handleFilterChanged(this._filter);
         }
     }
 }
