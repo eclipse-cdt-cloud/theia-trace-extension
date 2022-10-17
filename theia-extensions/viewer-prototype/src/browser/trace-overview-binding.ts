5d8c66e37a3202b4df98fde079a85a75712b4d73
Hoang Thuan Pham <hoang.pham@calian.ca>
2022-10-17 17:13:05 -0400

    Add configurable default view for the trace overview
    Currently, the trace overview default view is hard-coded in the trace
    viewer. This commit allows users/developers to set an XY view as the
    default view for the trace overview (using its name).
    
    Users can set the default view for the trace overview using the
    Preference UI (File > Preferences > Open Settings or Ctrl + ,).
    
    Developers can also set the default view for a specific Theia build by
    adding the "trace Viewer.trace Overview.defaultView" property to the
    "preferences" object in the package.json file of the build and indicate
    the name of the default view.
    
    Fixes #840.
    
    Signed-off-by: Hoang Thuan Pham <hoang.pham@calian.ca>

---
+++ theia-extensions/viewer-prototype/src/browser/trace-overview-binding.ts
@@ -0,0 +1,13 @@
+import { interfaces } from 'inversify';
+import { PreferenceService, createPreferenceProxy, PreferenceContribution } from '@theia/core/lib/browser';
+import { OverviewPreferences, OverviewSchema } from './trace-overview-preference';
+
+export function bindTraceOverviewPreferences(bind: interfaces.Bind): void {
+    bind(OverviewPreferences).toDynamicValue(ctx => {
+        const preferences = ctx.container.get<PreferenceService>(PreferenceService);
+        return createPreferenceProxy(preferences, OverviewSchema);
+    }).inSingletonScope();
+    bind(PreferenceContribution).toConstantValue({
+        schema: OverviewSchema,
+    });
+}
---
+++ theia-extensions/viewer-prototype/src/browser/trace-overview-preference.ts
@@ -0,0 +1,35 @@
+import { PreferenceProxy, PreferenceSchema, PreferenceScope } from '@theia/core/lib/browser';
+
+export const TRACE_OVERVIEW_DEFAULT_VIEW_KEY = 'trace Viewer.trace Overview.defaultView';
+export const DEFAULT_OVERVIEW_OUTPUT_NAME = 'Histogram';
+
+export function getSwitchToDefaultViewErrorMessage(preferredName: string, defaultName: string): string {
+    return `The ${preferredName} view cannot be opened as the trace overview. ` +
+    `Opening ${defaultName} instead. ` +
+    'Please set the Default View preference (Ctrl + ,) of the Trace Overview to an XY view, ' +
+    'or make sure the name is spelled correctly.';
+}
+
+export function getOpenTraceOverviewFailErrorMessage(): string {
+    return 'An error has occurred while opening the trace overview.';
+}
+
+export const OverviewSchema: PreferenceSchema = {
+    scope: PreferenceScope.Folder,
+    type: 'object',
+    properties: {
+        [TRACE_OVERVIEW_DEFAULT_VIEW_KEY]: {
+            type: 'string',
+            default: DEFAULT_OVERVIEW_OUTPUT_NAME,
+            description: 'Specify the name of the view that will be used as the default view for the Trace Overview. ' +
+            'Use the same name displayed in the Available Views list. E.g: For the Histogram view, enter Histogram.'
+        }
+    },
+};
+
+interface OverviewContribution {
+    [TRACE_OVERVIEW_DEFAULT_VIEW_KEY]: string
+}
+
+export const OverviewPreferences = Symbol('OverviewPreferences');
+export type OverviewPreferences = PreferenceProxy<OverviewContribution>;
