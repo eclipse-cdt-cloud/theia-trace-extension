b7cc4f15c978dd1378e74650a1d01f4aa8cf2f6e
hriday-panchasara <hriday.panchasara@ericsson.com>
2023-02-28 17:13:57 -0600

    Export DataTree to csv through FileDialogService
    Using Theia Backend to write files instead
    
    Users can choose file name/path in both browser & electron
    
    Show success/error message after
    
    Signed-off-by: hriday-panchasara <hriday.panchasara@ericsson.com>

--- theia-extensions/viewer-prototype/src/common/backend-file-service.ts
+++ theia-extensions/viewer-prototype/src/common/backend-file-service.ts
@@ -1,8 +1,9 @@
-import { CancellationToken } from '@theia/core';
+import { CancellationToken, URI } from '@theia/core';
 
 export const backendFileServicePath = '/services/theia-trace-extension/backend-service';
 export const BackendFileService = Symbol('BackendFileService');
 
 export interface BackendFileService {
     findTraces(uri: string, cancellationToken: CancellationToken): Promise<string[]>;
+    writeToFile(uri: URI, data: string): Promise<string>;
 }
