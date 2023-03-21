17788af37048690988e281fa5b227b1e7dde2102
hriday-panchasara <hriday.panchasara@ericsson.com>
2023-03-21 16:40:47 -0500

    Rename csv export signal
    Make csv export signal more generic so that it can be used by other outputs in the future
    
    Signed-off-by: hriday-panchasara <hriday.panchasara@ericsson.com>

--- packages/react-components/src/components/datatree-output-component.tsx
+++ packages/react-components/src/components/datatree-output-component.tsx
@@ -216,7 +216,7 @@ export class DataTreeOutputComponent extends AbstractOutputComponent<AbstractOut
                     csvArray.push(row.join(','));
                 }
                 const tableString = csvArray.join('\n');
-                signalManager().fireSaveDatatreeAsCsv({
+                signalManager().fireSaveAsCsv({
                     traceId: this.props.traceId,
                     data: tableString
                 });
