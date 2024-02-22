1abd87ffad36c51bc5702bccec63af8c62b15355
williamsyang-work <william.yang@ericsson.com>
2024-02-22 23:25:30 +0100

    Add Server Status Change Listeners
    Adds methods to add and remove functions that are executed when the
    server status is updated.
    
    Signed-off-by: Will Yang <william.yang@ericsson.com>

--- theia-extensions/viewer-prototype/src/browser/trace-server-connection-status-client-impl.ts
+++ theia-extensions/viewer-prototype/src/browser/trace-server-connection-status-client-impl.ts
@@ -2,13 +2,17 @@
 import { injectable } from 'inversify';
 import { TraceServerConnectionStatusClient } from '../common/trace-server-connection-status';
 
+type Listener = (serverStatus: boolean) => void;
 @injectable()
 export class TraceServerConnectionStatusClientImpl implements TraceServerConnectionStatusClient {
     protected active = false;
+    protected lastStatus = false;
+    protected listeners: Listener[] = [];
 
     updateStatus(status: boolean): void {
         if (this.active) {
-            TraceServerConnectionStatusClientImpl.renderStatus(status);
+            this.lastStatus = status;
+            this.listeners.forEach(listener => listener(status));
         }
     }
 
@@ -18,6 +22,21 @@ export class TraceServerConnectionStatusClientImpl implements TraceServerConnect
 
     deactivate(): void {
         this.active = false;
+    }
+
+    addServerStatusChangeListener(listener: Listener): void {
+        this.listeners.push(listener);
+    }
+
+    removeServerStatusChangeListener(listener: Listener): void {
+        const index = this.listeners.indexOf(listener);
+        if (index) {
+            this.listeners.splice(index, 1);
+        }
+    }
+
+    getStatus(): boolean {
+        return this.lastStatus;
     }
 
     static renderStatus(status: boolean): void {
