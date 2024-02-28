bb87e0e0b08cf5e7c329a049751bd9ccc93e2f9b
Marc Dumais <marc.dumais@ericsson.com>
2024-02-28 10:39:39 -0500

    update following upstream Theia changes
    There were some recent Theia changes[1] that required to change the way our
    Theia extension starts. It used to be that our TraceServerUrlProviderImpl
    backend contribution could wait in the "initialize" Theia app life-cycle
    phase, until the application was started and it received the trace server
    port from the front-end, from our PreferencesFrontendContribution.
    
    But the above no longer works. Now backend contributions are started in
    parallel, and all are expected to finish their initialize phase, before the
    "start" phase is initiated.
    
    In practice, after upgrading to Theia v1.45.1, the Theia application, that
    includes the trace viewer extension,  would be killed during startup, with
    no clear indication why.
    
    To prevent this from happening, the need to receive information from the
    front-end, during initialization, has been removed: the backend contribution
    now initializes with the assumption that the default trace server port is used.
    If a different port is configured in the preferences, the information will be
    synched after the app has started.
    
    [1]: https://github.com/eclipse-theia/theia/pull/12818
    
    specifically:
    
    "The BackendApplicatinContribution init and configure methods are now called
    in parallel for all contributions instead of sequentially."
    
    Signed-off-by: Marc Dumais <marc.dumais@ericsson.com>

--- theia-extensions/viewer-prototype/src/browser/preferences-frontend-contribution.ts
+++ theia-extensions/viewer-prototype/src/browser/preferences-frontend-contribution.ts
@@ -1,6 +1,6 @@
 import { FrontendApplicationContribution } from '@theia/core/lib/browser';
 import { inject, injectable } from 'inversify';
-import { PortPreferenceProxy } from '../common/trace-server-url-provider';
+import { PortPreferenceProxy, TRACE_VIEWER_DEFAULT_PORT } from '../common/trace-server-url-provider';
 import { TracePreferences, TRACE_PORT } from './trace-server-preference';
 
 @injectable()
@@ -12,7 +12,15 @@ export class PreferencesFrontendContribution implements FrontendApplicationContr
 
     async initialize(): Promise<void> {
         this.tracePreferences.ready.then(() => {
-            this.portPreferenceProxy.onPortPreferenceChanged(this.tracePreferences[TRACE_PORT]);
+            // assume the backend starts with the default server port - if the user configured
+            // a different port in the preferences, tell backend to change it
+            const tracePortPref = this.tracePreferences[TRACE_PORT];
+            if (tracePortPref === TRACE_VIEWER_DEFAULT_PORT) {
+                this.portPreferenceProxy.onPortPreferenceChanged(tracePortPref);
+            } else {
+                this.portPreferenceProxy.onPortPreferenceChanged(tracePortPref, TRACE_VIEWER_DEFAULT_PORT, true);
+            }
+
             this.tracePreferences.onPreferenceChanged(async event => {
                 if (event.preferenceName === TRACE_PORT) {
                     const newValue = typeof event.newValue === 'string' ? parseInt(event.newValue) : event.newValue;
