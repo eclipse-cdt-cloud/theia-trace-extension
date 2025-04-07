54c29510694f15876fac3f5100e5946cfc38ca0e
Marc Dumais <marc.dumais@ericsson.com>
2025-04-07 14:45:20 -0400

    Update for Theia community release v1.58.4
    Issue encountered:
    https://github.com/eclipse-theia/theia/issues/14804
    
    A PR updated "fix-path" and caused the issue:
    https://github.com/eclipse-theia/theia/pull/14781
    
    A fix was done:
    https://github.com/eclipse-theia/theia/pull/14812
    
    Side-effect of the fix:
    https://github.com/eclipse-theia/theia/issues/14804#issuecomment-2630660868
    
    Fix to address the side-effect:
    https://github.com/eclipse-theia/theia/pull/14819
    
    Note: the fix for the side-effect was merged on Theia master,
    and does not help momentariry on the community release 1.58.x,
    on which it's not currently planned to back-port this fix:
    https://github.com/eclipse-theia/theia/pull/14819#issuecomment-2786415441
    
    We could contribute a PR to back-port the fix, but I do not
    think this is super important for us - the Theia Electron app
    is only an example application and permanently switching to
    a bundled Electron backend does not seem to have any downside.
    
    In consequence, this PR includes using the bundled Electron
    backend, which is a simple webpack config update. If we later
    notice issues with this, we can roll-back to use the non-bundled
    backend in the next community release, which will have the fix
    above.
    
    Note about the webpack config update: as suggested, I have
    deleted the webpack configs and run "theia build" to have
    them re-created. Then I re-added the small change we need
    for the time range context menu. The result is a webpack
    config that's more aligned with the one Theia generates,
    for both our example Theia apps.
    
    Signed-off-by: Marc Dumais <marc.dumais@ericsson.com>

--- examples/browser/webpack.config.js
+++ examples/browser/webpack.config.js
@@ -3,23 +3,30 @@
  * To reset delete this file and rerun theia build again.
  */
 // @ts-check
-const config = require('./gen-webpack.config.js');
+const configs = require('./gen-webpack.config.js');
+const nodeConfig = require('./gen-webpack.node.config.js');
 const webpack = require("webpack");
-
 
 /**
  * Expose bundled modules on window.theia.moduleName namespace, e.g.
  * window['theia']['@theia/core/lib/common/uri'].
  * Such syntax can be used by external code, for instance, for testing.
-config.module.rules.push({
+configs[0].module.rules.push({
     test: /\.js$/,
     loader: require.resolve('@theia/application-manager/lib/expose-loader')
 }); */
 
-config[0].plugins.push(new webpack.DefinePlugin({
-  'process.env': {
-    NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development')
-  }
-}));
+// necessary for "time range" context menu to work at runtime. This is due to 
+// dependency react-contexify, that expects to see environment variable 
+// "NODE_ENV" defined. See:
+// https://github.com/eclipse-cdt-cloud/theia-trace-extension/issues/1066#issuecomment-2045774804
+configs[0].plugins.push(new webpack.DefinePlugin({
+    'process.env': {
+      NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development')
+    }
+  }));
 
-module.exports = config;
+module.exports = [
+  ...configs,
+  nodeConfig.config
+];
