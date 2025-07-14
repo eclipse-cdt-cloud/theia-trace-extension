7e2a88fad00785add877d977ff2450e3b8a6a1ee
Marc Dumais <marc.dumais@ericsson.com>
2025-07-14 13:51:15 -0400

    remove original traceviewerlibraries
    Signed-off-by: Marc Dumais <marc.dumais@ericsson.com>

--- traceviewer-libs/base/src/signals/time-range-data-signal-payloads.tsx
+++
@@ -1,6 +0,0 @@
-import { TimeRange } from '../utils/time-range';
-
-export interface TimeRangeUpdatePayload {
-    experimentUUID: string;
-    timeRange?: TimeRange;
-}
--- traceviewer-libs/react-components/jest-shim.ts
+++
@@ -1,6 +0,0 @@
-import { TextEncoder } from 'util';
-/*
- * Add TextEncoder from NodeJS util as it is
- * no longer available for Jest tests after React 18.
- */
-global.TextEncoder = TextEncoder;
